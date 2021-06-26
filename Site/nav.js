3ac64d3abdb76e07c861ef15a51f69023ca19a4c
Максим Василев <MDVasilev20@codingburgas.bg>
2021-06-26 22:14:28 +0300

    Small changes to all css files and about us

--- Site/nav.js
+++ Site/nav.js
@@ -1,7 +1,7 @@
 document.write(`<nav id="navbar">
 <ul id="navbar_list">
-    <img src="Images/favicon-logo.png" id="logo_nav">
-    <li><a class="navbar_item" class="active" href="index.html">Home Page</a></li>
+    <img src="Images/favicon-logo.png" class="logo_nav">
+    <li><a class="navbar_item" href="index.html">Home Page</a></li>
     <li><a class="navbar_item" href="About-Us.html">About Us</a></li>
     <li><a class="navbar_item" href="#">Login Form</a></li>
     <li><a class="navbar_item" href="Download-page.html">Download Page</a></li>
