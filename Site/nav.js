d8a391c55f94b8e9a26c06ae7979d968cece0449
Максим Василев <MDVasilev20@codingburgas.bg>
2021-06-26 16:59:17 +0300

    make navbar into a js file. Add our team page. Changes to Our team

---
+++ Site/nav.js
@@ -0,0 +1,10 @@
+document.write(`<nav id="navbar">
+<ul id="navbar-list">
+    <img src="Images/favicon-logo.png" id="logo-nav">
+    <li><a class="navbar-item" class="active" href="index.html">Home Page</a></li>
+    <li><a class="navbar-item" href="About-Us.html">About Us</a></li>
+    <li><a class="navbar-item" href="#">Login Form</a></li>
+    <li><a class="navbar-item" href="Download-page.html">Download Page</a></li>
+    <li><a class="navbar-item" href="Our-team.html">Our Team</a></li>
+</ul>
+</nav>`);
