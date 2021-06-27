3728bcf393e27f5fbb2cc0e70cf89e5db2ee15bc
Максим Василев <MDVasilev20@codingburgas.bg>
2021-06-28 00:53:05 +0300

    Fixed compatibility for Download Page

--- Site/nav.js
+++ Site/nav.js
@@ -3,7 +3,6 @@ document.write(`<nav id="navbar">
     <img src="Images/favicon-logo.png" class="logo_nav">
     <li><a class="navbar_item" href="index.html">Home Page</a></li>
     <li><a class="navbar_item" href="About-Us.html">About Us</a></li>
-    <li><a class="navbar_item" href="#">Login Form</a></li>
     <li><a class="navbar_item" href="Download-page.html">Download Page</a></li>
     <li><a class="navbar_item" href="Our-team.html">Our Team</a></li>
 </ul>
